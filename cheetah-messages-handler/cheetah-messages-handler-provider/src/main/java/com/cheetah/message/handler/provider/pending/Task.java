b9d7d259f1c42badf5aa1b491eec8ca5eaa5a676
longyun <longyun@linklogis.com>
2022-07-22 18:01:12 +0800

    完成 Task.java 部分业务逻辑

--- cheetah-messages-handler/cheetah-messages-handler-provider/src/main/java/com/cheetah/message/handler/provider/pending/Task.java
+++ cheetah-messages-handler/cheetah-messages-handler-provider/src/main/java/com/cheetah/message/handler/provider/pending/Task.java
@@ -1,10 +1,14 @@
 package com.cheetah.message.handler.provider.pending;
 
 
+import cn.hutool.core.collection.CollUtil;
 import com.cheetah.message.common.domain.TaskInfo;
+import com.cheetah.message.handler.provider.discard.DiscardMessageService;
+import com.cheetah.message.handler.provider.service.HandlerHolderProvider;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -19,7 +23,7 @@ import org.springframework.stereotype.Component;
  * @author longyun
  * @version 1.0
  * @date 2022/7/20 21:48
- * TODO: task任务未写
+ * TODO: task任务只完成部分，还欠缺屏蔽消息、去重功能
  */
 @Data
 @Accessors(chain = true)
@@ -30,11 +34,25 @@ public class Task extends Thread{
 
     private TaskInfo taskInfo;
 
+    @Autowired
+    private DiscardMessageService discardMessageService;
+
+    @Autowired
+    private HandlerHolderProvider holderProvider;
 
 
     @Override
     public void run() {
 
+        // 1. 判断是否丢弃消息
+        if(discardMessageService.isDiscard(taskInfo)){
+            return;
+        }
+
+        // 2. 发送消息
+        if(CollUtil.isNotEmpty(taskInfo.getReceiver())){
+            holderProvider.route(taskInfo.getSendChannel()).doHandler(taskInfo);
+        }
 
 
     }
