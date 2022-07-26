1a9e560e001cf5621881ab890aaf1485d139136e
longyun <longyun@linklogis.com>
2022-07-26 19:32:07 +0800

    启动类添加@EnabDubbo与忽视数据源注解

--- cheetah-messages-controller/cheetah-messages-controller-provider/src/main/java/com/cheetah/messages/controller/provider/CheetahMessagesControllerProviderApplication.java
+++ cheetah-messages-controller/cheetah-messages-controller-provider/src/main/java/com/cheetah/messages/controller/provider/CheetahMessagesControllerProviderApplication.java
@@ -1,9 +1,14 @@
 package com.cheetah.messages.controller.provider;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableDubbo(scanBasePackages = "com.cheetah.messages.controller.provider")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class CheetahMessagesControllerProviderApplication {
 
     public static void main(String[] args) {
