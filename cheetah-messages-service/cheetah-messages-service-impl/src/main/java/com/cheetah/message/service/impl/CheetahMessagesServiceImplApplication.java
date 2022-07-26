b34055c4c7a2670682f0ea3dcd54ac087de09b19
longyun <longyun@linklogis.com>
2022-07-26 19:22:02 +0800

    修复一些小细节

--- cheetah-messages-service/cheetah-messages-service-impl/src/main/java/com/cheetah/message/service/impl/CheetahMessagesServiceImplApplication.java
+++ cheetah-messages-service/cheetah-messages-service-impl/src/main/java/com/cheetah/message/service/impl/CheetahMessagesServiceImplApplication.java
@@ -1,9 +1,11 @@
 package com.cheetah.message.service.impl;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo(scanBasePackages = "com.cheetah.message.service.impl")
 public class CheetahMessagesServiceImplApplication {
 
     public static void main(String[] args) {
